6e8f7cb0e43381245e3033f2c58e514519b5ec58
Kilperic <kilpericn@gmail.com>
2020-08-27 09:57:05 -0400

    Prg Adaptation for 2 inputs noised - plot

--- examples/muscle_driven_ocp/muscle_excitation_tracking+noise/Prg_N_Exci-Mark/plot.py
+++ examples/muscle_driven_ocp/muscle_excitation_tracking+noise/Prg_N_Exci-Mark/plot.py
@@ -9,22 +9,23 @@ import MainPgr as MP
 ## Import Information ###
 
 biorbd_model = biorbd.Model(MP.model)
-final_time = MP.final_time
 ns = MP.n_shooting_points
 n_q = biorbd_model.nbQ()
 n_qdot = biorbd_model.nbQdot()
 n_mark = biorbd_model.nbMarkers()
 n_muscl = biorbd_model.nbMuscleTotal()
-PctB = MP.PourcentBase
-PctM = MP.PorcentMany
+PctBE = MP.PourcentBaseE
+PctBM = MP.PourcentBaseM
+PctME = MP.PorcentManyE
+PctMM = MP.PorcentManyM
 DEN = MP.DataEssaieName
 Nb_DEN = len(DEN)
-PctE = MP.PorcentEssaie
+PctEss = MP.PorcentEssaie
 tf = MP.final_time
 
 # Load error Data
 Er_Exci = np.load('RE_er_exci.npy') * (100/n_muscl)             # 100 : Pour avoir des pourcentage
-#Er_Exci_Vir = np.load('RE_er_exci_vir.npy') * (100/n_muscl)     # 100 : Pour avoir des pourcentage
+Er_Exci_Vir = np.load('RE_er_exci_vir.npy') * (100/n_muscl)     # 100 : Pour avoir des pourcentage
 EE = Er_Exci.tolist()
 #EEV = Er_Exci_Vir.tolist()
 Er_Q = np.load('RE_er_q.npy')
@@ -35,6 +36,12 @@ Er_Mak_Z = np.load('RE_er_mark_Z.npy') * (100/n_mark)
 EMX = Er_Mak_X.tolist()
 EMY = Er_Mak_Y.tolist()
 EMZ = Er_Mak_Z.tolist()
+Er_Mak_Vir_X = np.load('RE_er_mark_vir_X.npy') * (100/n_mark)   # 100 : Pour avoir des pourcentage
+Er_Mak_Vir_Y = np.load('RE_er_mark_vir_Y.npy') * (100/n_mark)
+Er_Mak_Vir_Z = np.load('RE_er_mark_vir_Z.npy') * (100/n_mark)
+EMVX = Er_Mak_Vir_X.tolist()
+EMVY = Er_Mak_Vir_Y.tolist()
+EMVZ = Er_Mak_Vir_Z.tolist()
 
 def Rang(i):
     R = ['1st', '2nd', '3rd']
@@ -53,14 +60,15 @@ def Add_List(L, K, Start = 0):
 # Boxplot for Excitation
 Data = []
 Data2 = []
-BoxName = [f'{i * PctB}%' for i in range(PctM)]
+BoxName = [f'{i * PctBE}%' for i in range(PctME)]
 for j in range(Nb_DEN):
     plt.subplot(1, Nb_DEN, 1 + j)
     data = []
-    for i in range(PctM):
-        data.append(Add_List(EE, PctE, Start = i*5 + j*20))
+    for i in range(PctME):
+        data.append(Add_List(EE, PctEss, Start = (i*PctEss + j*(PctME*PctEss*PctMM))))
+        # On veut l'ecart avec les differents % de bruits de E, symbolisé par i, avec les 3 données bruts, j, chacune écarté (Nb Jeu de bruit M (4) * Nb Jeu de bruit E (3)) * Nb Jeu d'essaie (3) = 36)
     plt.boxplot(data)
-    pylab.xticks([h+1 for h in range(PctM)], BoxName)
+    pylab.xticks([h+1 for h in range(PctME)], BoxName)
     plt.title(f'Data set : {Rang(j)} best out of 28', fontsize='20')
     plt.ylabel("% of error of excitation by Muscle and by Increment", fontsize='14')
     plt.xlabel("Levels of noises", fontsize='14')
@@ -75,20 +83,10 @@ D = np.array(Data)
 # MeanD = [D[:, i].mean() for i in range(4)]
 plt.figure()
 plt.boxplot(np.concatenate((D2), axis = 0))
-pylab.xticks([h+1 for h in range(PctM)], BoxName)
+pylab.xticks([h+1 for h in range(PctME)], BoxName)
 plt.title(f'Thrit best out of 28 data set', fontsize='20')
 plt.ylabel("% of error of excitation by Muscle and by Increment", fontsize='14')
 plt.xlabel("Levels of noises", fontsize='14')
-plt.show()
-
-# Boxplot for Q
-BoxName = ['Erreur of Q']
-data = [Er_Q[0]]
-plt.boxplot(data)
-# plt.ylim(-0.1, 1.1)
-pylab.xticks([1], BoxName)
-# plt.savefig('MultipleBoxPlot02.png')
-# plt.title('Velocity of the optimisation')
 plt.show()
 
 # Boxplot for error markeurs x, y, z
@@ -103,15 +101,16 @@ pylab.xticks([1, 2, 3], BoxName)
 # plt.title('Velocity of the optimisation')
 plt.show()
 
-BoxName = [f'{i * PctB}%' for i in range(PctM)]
+BoxName = [f'{i * PctBM}%' for i in range(PctMM)]
 Data3 = []
 for j in range(Nb_DEN):
     plt.subplot(1, Nb_DEN, 1 + j)
     data = []
-    for i in range(PctM):
-        data.append(Add_List(EMX, PctE, Start = i*5 + j*20))
+    for i in range(PctMM):
+        data.append(Add_List(EMX, PctEss, Start = (i*(PctEss*PctME) + j*(PctME*PctEss*PctMM))))
+        # L'ecart pour i, entre 2 bruit de marqueur, est de (Nb de Jeux * Nb de Bruit Excitation)
     plt.boxplot(data)
-    pylab.xticks([h+1 for h in range(PctM)], BoxName)
+    pylab.xticks([h+1 for h in range(PctMM)], BoxName)
     plt.title(f'Data set : {Rang(j)} best out of 28')
     if j == 0:
         plt.ylabel("% of error of X-axis for each marker by Increment")
@@ -125,7 +124,7 @@ D3 = np.array(Data3)
 # MeanD = [D[:, i].mean() for i in range(4)]
 plt.figure()
 plt.boxplot(np.concatenate((D3), axis = 0))
-pylab.xticks([h+1 for h in range(PctM)], BoxName)
+pylab.xticks([h+1 for h in range(PctMM)], BoxName)
 plt.title(f'Thrit best out of 28 data set', fontsize='20')
 plt.ylabel("% of error of X-axis for each marker by Increment", fontsize='14')
 plt.xlabel("Levels of noises", fontsize='14')
@@ -133,48 +132,39 @@ plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 3))
 plt.show()
 
 
-##   - Plot Excitation pour presentation -   ##
+## Pour être sur qu'on ai un ecart entre les marqueurs bruité et ceux optimisé
+## On voit que c'est linéaire, avec une distribution uniforme, comme le bruit imposé, donc parfait
+## Sert a verifier qu'on applique bien le bon bruit
 
-# Chemin Originaux
 
-pathRef = MP.pathRef + '/' + DEN[0]
-DRef = MP.DataRef
-RefData = np.ones((len(DRef)))
-RefData2 = RefData.tolist()
+BoxName = [f'{i * PctBM}%' for i in range(PctMM)]
+Data4 = []
+for j in range(Nb_DEN):
+    plt.subplot(1, Nb_DEN, 1 + j)
+    data = []
+    for i in range(PctMM):
+        data.append(Add_List(EMVX, PctEss, Start = (i*(PctEss*PctME) + j*(PctME*PctEss*PctMM))))
+    plt.boxplot(data)
+    pylab.xticks([h+1 for h in range(PctMM)], BoxName)
+    plt.title(f'Data set : {Rang(j)} best out of 28')
+    if j == 0:
+        plt.ylabel("% of error of X-axis for each marker by Increment")
+    plt.xlabel("Levels of noises")
+    Data4.append(np.array(data).T)
+# plt.savefig('Exci_noises-EE.png')
+plt.gcf().subplots_adjust(left = 0.07, bottom = 0.07, right = 0.975, top = 0.95, wspace = 0.18, hspace = 0.25)
+# plt.show()
 
-# Ref part
+D4 = np.array(Data4)
+# MeanD = [D[:, i].mean() for i in range(4)]
+plt.figure()
+plt.boxplot(np.concatenate((D4), axis = 0))
+pylab.xticks([h+1 for h in range(PctMM)], BoxName)
+plt.title(f'Error of X-axis for each marker by Increment compare to input noises data', fontsize='20')
+plt.ylabel("% of error of X-axis for each marker by Increment", fontsize='14')
+plt.xlabel("Levels of noises", fontsize='14')
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 3))
+plt.ylim(-0.1, 4)
+# plt.show()
 
-for j, R in enumerate(DRef):
-    path2 = os.path.join(pathRef, R)
-    if os.path.exists(path2):
-        RefData2[j] = np.load(path2)
 
-pathData = MP.pathRef + '/' + DEN[0] +'/DR1.5'
-
-List_Data_Ramd = [[0]]
-Donner = ['r-activations.npy', 'r-tau.npy', 'r-q_dot.npy', 'r-excitations.npy', 'r-q.npy', 'r-nexci.npy']
-for d, D in enumerate(Donner):
-    pathRamd2 = os.path.join(pathData, D)
-    if os.path.exists(pathRamd2):
-        List_Data_Ramd[d] = np.load(pathRamd2)
-        List_Data_Ramd.append([0])
-q = List_Data_Ramd[4]
-q_dot = List_Data_Ramd[2]
-activations = List_Data_Ramd[0]
-tau = List_Data_Ramd[1]
-excitations = List_Data_Ramd[3]
-new_exci = List_Data_Ramd[5:]
-new_exci2 = np.concatenate(([new_exci[0][0, 1]], new_exci[0][:-1, 1]), axis=0)
-
-muscle_excitations_ref = RefData2[0]
-
-plt.figure("Muscle excitations")
-plt.step(np.linspace(0, tf, ns + 1), muscle_excitations_ref[:, 1], "k", where="post", label = 'Initial Input')
-plt.step(np.linspace(0, tf, ns + 1), new_exci[0][:, 1], 'g*', where="post", label = 'Noises Input')
-plt.step(np.linspace(0, tf, ns + 1), new_exci2, 'g*', where="post")
-plt.step(np.linspace(0, tf, ns + 1), excitations.T[:, 1], color='r', ls ='--', where="post", label = 'Optimisation Resust')
-plt.legend(loc = 'best', fontsize='12')
-plt.xlabel("Time (s)", fontsize='14')
-plt.ylabel("Excitation values", fontsize='14')
-plt.title(f'Muscle excitation', fontsize='20')
-plt.show()
